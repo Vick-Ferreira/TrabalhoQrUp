00014893c6c3d9ae384585785e631577a597d6ed
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2023-11-08 14:20:35 -0300

    add

--- serverWeb/Web/js/alunos.js
+++ serverWeb/Web/js/alunos.js
@@ -1,3 +1,6 @@
+// Variável de controle para verificar se o código QR já foi gerado
+let qrCodeGerado = false;
+
 // Função para recuperar o CPF da URL
 function getCPFFromURL() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -6,27 +9,31 @@ function getCPFFromURL() {
 
 // Função para gerar o código QR com base no CPF
 function gerarQRCode() {
+  // Verifique se o código QR já foi gerado
+  if (qrCodeGerado) {
+    return; // Se já foi gerado, saia da função
+  }
+
+  // Recupere o CPF da URL
   const cpf = getCPFFromURL();
 
   if (cpf) {
-      console.log("CPF Capturado:", cpf);
+    // Exibe o CPF capturado no console
+    console.log("CPF Capturado:", cpf);
 
-      // Limpe o conteúdo anterior do elemento #qrcode
-      document.getElementById("qrcode").innerHTML = "";
+    // Gere o código QR com base no CPF
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+      text: cpf,
+      width: 128,
+      height: 128
+    });
 
-      // Gere o código QR com base no CPF
-      var qrcode = new QRCode(document.getElementById("qrcode"), {
-          text: cpf,
-          width: 128,
-          height: 128
-      });
+    // Defina a variável de controle para true para indicar que o código QR foi gerado
+    qrCodeGerado = true;
   } else {
-      alert("CPF não encontrado na URL.");
+    alert("CPF não encontrado na URL.");
   }
 }
 
-// Associe a função ao evento de clique do botão "IMPRIMA SEU QRCOD"
+// Associe a função ao evento de clique do botão
 document.getElementById("generateQRCode").addEventListener("click", gerarQRCode);
-
-// Execute a função para gerar o código QR assim que a página for carregada
-gerarQRCode();
--- serverWeb/Web/js/index.js
+++ serverWeb/Web/js/index.js
@@ -1,12 +1,20 @@
+
 const btnCriar = document.getElementById("criar");
 const btnBuscar = document.getElementById("buscar");
 
+
+
+// Manipulador de evento para o botão "Criar"
 btnCriar.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Impede o envio padrão do formulário
+
+    // Buscando os dados
     const cpf = document.getElementById("InputCPF").value;
     const senha = document.getElementById("InputSenha").value;
 
-    function criarAluno(cpf, senha) {
+    // Criando função para criar um aluno
+    function CriarAluno(cpf, senha) {
+        // Enviando os dados (CPF e senha) para o servidor e recebendo a resposta.
         fetch("https://json-qrcod.vercel.app/alunos", {
             method: "POST",
             headers: {
@@ -17,29 +25,38 @@ btnCriar.addEventListener("click", (e) => {
         .then((resp) => resp.json())
         .then((data) => {
             alert("Aluno CRIADO.");
-            gerarQRCode(cpf);
-            console.log("Aluno criado");
+            // Gere o código QR com base no CPF inserido
+            var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: cpf, // Use o CPF como texto para o código QR
+            width: 128,
+            height: 128
+        });
+            console.log("Aluno criado"); // Exibe no console
         })
         .catch((error) => {
             console.log("Erro na solicitação: " + error);
         });
     }
-
-    criarAluno(cpf, senha);
+    
+    // Chame a função CriarAluno
+    CriarAluno(cpf, senha);
 });
 
+// Manipulador de evento para o botão "Buscar"
 btnBuscar.addEventListener("click", (e) => {
     e.preventDefault();
+
     const cpf = document.getElementById("InputCPF").value;
     const senha = document.getElementById("InputSenha").value;
 
-    function verificarLogin(cpf, senha) {
+    function VerificarLogin(cpf, senha) {
         fetch(`https://json-qrcod.vercel.app/alunos?cpf=${cpf}&senha=${senha}`)
             .then((resp) => resp.json())
             .then((data) => {
                 if (data.length > 0) {
                     // Aluno encontrado, redirecionar para a página "Alunos.html" com o CPF como parâmetro
                     window.location.href = `./Alunos.html?cpf=${cpf}`;
+                
                 } else {
                     // Aluno não encontrado, exibir mensagem de erro
                     alert("Aluno não encontrado. Verifique o CPF e a senha.");
@@ -49,6 +66,10 @@ btnBuscar.addEventListener("click", (e) => {
                 console.error("Erro na solicitação: " + error);
             });
     }
-
-    verificarLogin(cpf, senha);
+    
+    
+    // Chame a função para verificar o login
+    VerificarLogin(cpf, senha);
 });
+
+
